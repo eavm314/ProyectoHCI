a72078a2481167e8c6abb8eb9b33e88ba9df537e
enrique <enriqueadhemar@gmail.com>
2023-05-10 19:58:40 -0400

    Perfil ajustable

--- src/components/Home/Perfil.jsx
+++ src/components/Home/Perfil.jsx
@@ -4,14 +4,16 @@ import React from 'react'
 export const Perfil = () => {   
   return (
     <button 
-    className='group flex items-center bg-rojito-2 m-2 p-4 rounded-xl
-    hover:flex-col hover:justify-center'>
+    className='group flex items-center 
+    bg-rojito-2 m-2 p-4 rounded-xl
+    transition
+    focus:flex-col focus:justify-center'>
         <img 
         className='h-12 w-12 rounded-full 
-        group-hover:w-1/2 group-hover:h-auto'
-        src="./public/dory.png" alt="perfil" />
+        transition group-focus:w-1/2 group-focus:h-auto'
+        src="./dory.png" alt="perfil" />
 
-        <p className='text-4xl ml-2 mr-4 group-hover:mt-4 group-hover:text-5xl'>Dory</p>
+        <p className='text-4xl ml-2 mr-4 transition group-focus:mt-4 group-focus:text-5xl'>Dory</p>
 
         {/* <button className='ml-auto hover:text-rojito-5'>
             <IconEdit  size={28}/>
